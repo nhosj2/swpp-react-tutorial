ec1cca5f0ac8f9b0aaee268be9362032a38e2b9b
Jeong-ho Sohn <nhosj2@gmail.com>
2020-09-18 20:49:07 +0900

    lab3 done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import TodoList from './containers/TodoList/TodoList';
+import NewTodo from './containers/TodoList/NewTodo/NewTodo';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <Switch>
+                    <Route path='/todos' exact render={() => <TodoList title='My TODOs!' />} />
+                    <Route path='/new-todo' exact component={NewTodo} />
+                    <Redirect exact from ='/' to='/todos' />
+                    <Route render={() => <h1>Not Found</h1>} />
+                </Switch>
+            </div>
+        </BrowserRouter>
+    );
+};
 
 export default App;
